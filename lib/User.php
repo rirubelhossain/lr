b890b2e2cf989ec30f463127389d0c4301d295f6
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2022-04-04 12:21:48 +0600

    Register page done

--- lib/User.php
+++ lib/User.php
@@ -8,21 +8,95 @@ class User
 	function __construct()
 	{	
 		$this->db = new Database() ;
-		// $this->db->test();
+		 
 	}
 	public function userRegistration($data){
 		$name = $data['name'] ;
 		$username = $data['username'] ;
 		$email = $data['email'];
-		$pass = $data['password'];
+		$password = md5($data['password']);
+		$chk_email =  $this->emailChek($email);
 
-		echo $name ."<br>";
-		echo $username."<br>";
-		echo $email ."<br>";
-		echo $pass."<br>";
+		if($name  == "" or $username == "" or $email == "" or $password == "" ){
+			$msg = "<div class = 'alert alert-danger'>
+			<strong>
+			Error ! 
+			</strong>Field must not be empty 	
+			</div>";
+			return $msg ;
+		}
+		if(strlen($username) < 3){
+			$msg = "<div class = 'alert alert-danger'>
+			<strong>
+			Error ! 
+			</strong>username is too short  	
+			</div>";
+			return $msg ;
+		}
+		elseif(preg_match('/[^a-zA-Z0-9_-]+/i',$username) ){
+			$msg = "<div class = 'alert alert-danger'>
+			<strong>
+			Error ! 
+			</strong>Username must contain alphanumerical and dashes and Underscores! 	
+			</div>";
+			return $msg ;
+		}
+		if(filter_var($email , FILTER_VALIDATE_EMAIL) === false){
+			$msg = "<div class = 'alert alert-danger'>
+			<strong>
+			Error ! 
+			</strong>The Email address is not valid! 	
+			</div>";
+			return $msg ;	
+		}
+		if( $chk_email === true ){
+			$msg = "<div class = 'alert alert-danger'>
+			<strong>
+			Error ! 
+			</strong>The Email address is already Exist 	
+			</div>";
+			return $msg ;	
+		}
 
-		echo "Print from User page ";
+		$sql = "INSERT INTO tbl_user(name , username , email , password) VALUES(:name , :username , :email , :password )";
+		$query = $this->db->pdo->prepare($sql);
+		$query->bindValue(':name',$name);
+		$query->bindValue(':username',$username);
+		$query->bindValue(':email',$email);
+		$query->bindValue(':password',$password);
+		$result = $query->execute();
+
+		if($result){
+			$msg = "<div class = 'alert alert-success'>
+			<strong>
+			Success ! 
+			</strong>You have been Registerd! 	
+			</div>";
+			return $msg ;		
+		}else{
+			$msg = "<div class = 'alert alert-danger'>
+			<strong>
+			Sorry ! 
+			</strong>There has been problem inserting  	
+			</div>";
+			return $msg ;	
+		}
 	}
+
+	public function emailChek($email){
+		$sql = "SELECT email from tbl_user WHERE email = :email";
+		$query = $this->db->pdo->prepare($sql);
+		$query->bindValue(':email' , $email) ;
+		$query->execute();
+
+		if($query->rowCount() > 0 ){
+			return true ;
+		}else{
+			return false ;
+		}
+			
+		}
+	
 
 }
 
--- register.php
+++ register.php
@@ -10,37 +10,40 @@ include "lib/User.php";
 $user = new User();
 
 if($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_POST['register'])){
-	$uerRegi = $user->userRegistration($_POST);
-	 
+	///echo "hello connected ";
+	$usrRegi = $user->userRegistration($_POST);	 
 }
-
-
 ?>
-
-
 		<div class="panel panel-default">
 		<div class="panel-heading">
 			<h2>User Register</h2>
 		</div>
-
 		<div class="panel-body">
 			<div style="max-width: 600px; margin: 0 auto">
+<?php
+
+if (isset($usrRegi)) {
+	echo $usrRegi;
+}
+
+?>
+
 			 <form action="" method="POST">
 			 	<div class="form-group">
 			 		<label for="name">Your Name</label>
-			 		<input type="text" name="name" class="form-control" id="name" required="1" />
+			 		<input type="text" name="name" class="form-control" id="name"   />
 			 	</div>
 			 	<div class="form-group">
 			 		<label for="username">UserName</label>
-			 		<input type="text" name="username" class="form-control" id="username" required="1" />
+			 		<input type="text" name="username" class="form-control" id="username"   />
 			 	</div>
 			 	<div class="form-group">
 			 		<label for="email">Email Address</label>
-			 		<input type="email" name="email" class="form-control" id="email" required="1" />
+			 		<input type="email" name="email" class="form-control" id="email"   />
 			 	</div>
 			 	<div class="form-group">
 			 		<label for="password">Password</label>
-			 		<input type="password" name="password" class="form-control" id="password" required="1" />
+			 		<input type="password" name="password" class="form-control" id="password"   />
 			 	</div>
 			 	<button class="btn btn-success" type="submit" name="register">Submit</button>
 			 </form>
@@ -57,3 +60,8 @@ include 'inc/footer.php';
 		 
  
 
+<?php
+
+
+
+?>
